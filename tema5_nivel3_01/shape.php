a0f9510633ef73b9e041e25c27dd0ac170e425a3
frankpulido <frankpulido@me.com>
2024-08-07 19:15:30 +0200

    tema 5 poo2 finished

--- tema5_nivel3_01/shape.php
+++ tema5_nivel3_01/shape.php
@@ -3,6 +3,7 @@ interface Shape { // La implementarán todas las figuras geométricas.
 
 // Función ha implementar por cualquier Clase, no existirá una relación de "herencia"
 
+public function how(); // El método únicamente se declara. Se desarrollara en las clases que implementen Shape (implementación obligatoria)
 public function area(); // El método únicamente se declara. Se desarrollara en las clases que implementen Shape (implementación obligatoria)
 
 }
